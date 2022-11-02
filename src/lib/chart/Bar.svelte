48da7c27b8b29a21277bca18fd3c2169204f7d3b
Sören Maschmann <smaschmann@slashwhy.de>
2022-11-02 12:41:16 +0100

    fix: bar hover interaction

--- src/lib/chart/Bar.svelte
+++ src/lib/chart/Bar.svelte
@@ -12,5 +12,5 @@
 </script>
 
 <div class={`group relative h-[var(--barheight)] rounded ${highlight ? 'bg-cyan hover:bg-light-cyan' : 'bg-soft-red hover:bg-light-soft-red'} transition-colors duration-300`} bind:this={barElement}>
-  <span class="absolute -top-10 left-1/2 px-1 py-2 rounded bg-dark-brown text-sm text-very-pale-orange leading-none opacity-0 -translate-x-1/2 group-hover:opacity-100 transition-opacity duration-300">{`$${value}`}</span>
+  <span class="absolute -top-10 left-1/2 px-1 py-2 rounded bg-dark-brown text-sm text-very-pale-orange leading-none opacity-0 -translate-x-1/2 group-hover:opacity-100 transition-opacity duration-300 pointer-events-none">{`$${value}`}</span>
 </div>
